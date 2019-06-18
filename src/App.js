719213ce45c0b6f54e4d021adc349930f67215a9
Ikhwan Noor Ikhsan <23516049@std.stei.itb.ac.id>
2019-06-18 15:20:20 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Todos from "./components/todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [{ id: 1, content: "Beli susu" }, { id: 2, content: "Tidur" }]
+  };
+  render() {
+    return (
+      <div>
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/todos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todos = ({ todos }) => {
+  return <div className="todos collection" />;
+};
+
+export default Todos;
